90ca28df0b28966cd69c77cb87cba0841609ae1d
ZiqianXie <z.xie4@umiami.edu>
2019-05-08 13:45:07 -0500

    FGSM

--- viz.py
+++ viz.py
@@ -4,6 +4,7 @@ from torchvision.models import resnet18
 from PIL import Image
 import numpy as np
 from torch.nn.functional import interpolate
+from copy import deepcopy
 
 
 class GReLU(torch.autograd.Function):
@@ -16,8 +17,8 @@ class GReLU(torch.autograd.Function):
     def backward(ctx, grad_output):
         input, = ctx.saved_tensors
         grad_input = grad_output.clone()
-        grad_input[input < 0] = 0
-        grad_input = grad_input.clamp(min=0)
+        grad_input[input * grad_output < 0.02] = 0
+        # grad_input = grad_input.clamp(min=0)
         return grad_input
 
 
@@ -36,10 +37,6 @@ class GSigmoid(torch.autograd.Function):
         return grad_input
 
 
-class customGRU(nn.Module):
-    def __init__(self):
-
-
 class GuidedReLU(nn.Module):
     def __init__(self):
         super().__init__()
@@ -50,8 +47,8 @@ class GuidedReLU(nn.Module):
 
 class GuidedBackprop:
     def __init__(self, model):
-        self.model = model
-        self.replace(model)
+        self.model = deepcopy(model)
+        self.replace(self.model)
 
     def replace(self, container):
         for key, mod in container._modules.items():
@@ -110,26 +107,62 @@ class GradCAM:
         return heatmaps
 
 
-
-
-
-if __name__ == "__main__":
-    from matplotlib.pyplot import imshow,figure, colorbar
-    m = resnet18(pretrained=True)
-    m.eval()
-    # g = GuidedBackprop(m)
-    g = GradCAM(m, 48)
-    x = np.array(Image.open('cat_dog.png'))
-    imshow(x)
+def read_im(im_name, show=True):
+    from matplotlib.pyplot import imshow, figure
+    x = np.array(Image.open(im_name))
+    if show:
+        figure()
+        imshow(x)
     x = x.astype('f') / 255
     x -= np.array([0.485, 0.456, 0.406])
     x /= np.array([0.229, 0.224, 0.225])
-    x = torch.from_numpy(x.transpose(2, 0, 1)[None, ...])
-    d, = g.probe(x, 281)
-    # d[d < 0] = 0
-    # d = d.sum((0, 1)).numpy()
-    # d = (d - d.min())/(d.max() - d.min()) * 255
+    return torch.from_numpy(x.transpose(2, 0, 1)[None, ...])
+
+
+class FGSM:
+    def __init__(self, model, epsilon=1e-2):
+        self.model = model
+        self.model.eval()
+        self.epsilon = epsilon
+
+    def show(self, x):
+        from matplotlib.pyplot import imshow, figure
+        figure()
+        x = x.detach().numpy()[0].transpose(1, 2, 0)
+        # print(x.shape)
+        x = (x - x.min((0, 1)))/(x.max((0, 1), ) - x.min((0, 1))) * 255
+        imshow(x.astype('i'))
+
+    def adv_sample(self, x):
+        x = x.detach().clone().requires_grad_()
+        out = self.model(x)
+        orig_class = out.argmax(1)
+        cur_class = orig_class
+        mask = cur_class == orig_class
+        while mask.any():
+            self.model(x)[:, orig_class].backward()
+            x = x.detach() - self.epsilon * torch.sign(x.grad) * mask.float()
+            # self.show(x)
+            x.requires_grad_()
+            cur_class = self.model(x).argmax(1)
+            print(cur_class)
+            mask = cur_class == orig_class
+        self.show(x)
+        return x, cur_class
+
+
+if __name__ == "__main__":
+    from matplotlib.pyplot import imshow,figure # , colorbar
+    m = resnet18(pretrained=True)
+    m.eval()
+    g = GuidedBackprop(m)
+    # g = GradCAM(m, 48)
+    x = read_im("cat.jpg")
+    d = g.probe(x, 281)
+    d[d < 0] = 0
+    d = d[0].numpy()
+    d = (d - d.min())/(d.max() - d.min()) * 255
     figure()
-    # imshow(d.astype('i'))
-    imshow(d[0,0].detach().numpy())
-    colorbar()
+    imshow(d.transpose(1, 2, 0).astype('i'))
+    # imshow(d[0,0].detach().numpy())
+    #colorbar()
